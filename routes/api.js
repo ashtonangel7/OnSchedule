e7f73ee3de51295c98bef0142a82a827758ae9ba
Ashton <ashton7@live.com>
2017-08-09 22:00:29 +0200

    api test

--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,7 @@ let express = require('express');
 let router = express.Router();
 
 router.get('/', function (req, res, next) {
-    res.send("I like to party");
+    res.send(req);
 });
 
 module.exports = router;
