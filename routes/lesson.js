707ce81bd43b9081943ae01d9f577a6619e18615
Ashton <ashton7@live.com>
2017-08-13 09:46:13 +0200

    Lesson Page

--- routes/lesson.js
+++ routes/lesson.js
@@ -1,9 +1,15 @@
 let express = require('express');
 let router = express.Router();
 
+let lessonData = {
+    title: "Lesson",
+    students: ["Ashton", "Mike"],
+    startTimes: ["08:00","09:00","10:00","11:00"]
+};
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('lesson');
+    res.render('lesson', lessonData);
 });
 
 module.exports = router;
